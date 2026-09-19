7822f34e82c02b2a749969ead7fbae7b63c656f1
a1262225007-sketch <a1262225007@gmail.com>
2026-09-20 01:09:18 +0800

    Sync workdesk configuration

--- config.js
+++ config.js
@@ -11,3 +11,16 @@ window.WORKDESK_SITES = [
     url: "https://mail.163.com/"
   }
 ];
+window.WORKDESK_CONTENT_PATH = "data/content.json";
+window.WORKDESK_SITES = [
+  {
+    id: "erke",
+    name: "素拓平台",
+    url: "https://erke.gdut.edu.cn/public/index.php/cgi/login"
+  },
+  {
+    id: "mail",
+    name: "163邮箱",
+    url: "https://mail.163.com/"
+  }
+];
